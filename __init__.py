1a3948d1952cfce1f4c507db7120ab62c61ad239
Mathias Hauan Arbo <mathias.arbo@itk.ntnu.no>
2018-04-09 10:26:35 +0200

    Starting on ReactiveNLPController

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 #
 from constraints import EqualityConstraint, SetConstraint
 from skill_specification import SkillSpecification
-from controller import ReactiveQPController
+from controller import ReactiveQPController, ReactiveNLPController
